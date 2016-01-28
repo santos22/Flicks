d98408fecec1c7bd1406cbde0007010f29e40e4a
santos22 <santosjavier22@gmail.com>
2016-01-28 09:23:36 -0600

    Successfully performed te segue

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -154,14 +154,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "showOverviewSegue" {
+            let cell = sender as! UITableViewCell
+            if let indexPath = tableView.indexPathForCell(cell) {
+                let nameController = segue.destinationViewController as! OverviewViewController
+                let movie = movies![indexPath.row] // unwraps
+                let overview = movie["overview"] as! String
+                
+                nameController.overview = overview
+                tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            }
+        }
     }
-    */
 
 }
